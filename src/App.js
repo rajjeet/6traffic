98b85996d6a79c6144550f14d6bda29dbf504553
Rajjeet Phull <rajjeet.phull@gmail.com>
2018-12-05 13:32:57 -0500

    Added menu and placeholder for maps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+// import './App.css';
+import TextMenu from "./components/TextMenu";
+import {Grid, Segment, Image, Container} from "semantic-ui-react";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Grid columns={2}>
+                    <Grid.Column>
+                        <TextMenu/>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Segment>
+                            <Image src='https://via.placeholder.com/350'/>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+            </Container>
+        );
+    }
 }
 
 export default App;
